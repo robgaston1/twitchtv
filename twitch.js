bbaec73009b8f23a469641638ef7db25c7d7255f
robgaston1 <robcoach0221@gmail.com>
2017-01-16 16:05:18 -0500

    fixed up streaming user table, centered the texts vertically

--- twitch.js
+++ twitch.js
@@ -3,30 +3,37 @@ $(document).ready(function() {
 
     var userArray = ["freecodecamp", "brunofin", "streamerhouse", "insomniacgamers12345", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     
+   
+    function offline (user) {
+         $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?',function(data) {
+//             console.log("offline:", data);
+         });
+    } 
+    
+    function getInfo(user) {
+        var logo = "";
+        var link = "";
+        var name = "";
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+user+'?callback=?', function (data) {
+            if (data.stream==null) {
+              offline(user);  
+            } else {
+            console.log(data);
+            link = data.stream.channel.url;
+            logo = "<a href=\""+link+"\" target=\"blank\"><img src=\""+data.stream.channel.logo+"\"></a>";
+            name= data.display_name;
+            game = data.stream.channel.game;
+            viewers = data.stream.viewers;
+            $('.streaming').append("<div class=\"row\"><div class=\"col-md-3\">"+logo+"</div><div class=\"col-md-3\">"+user+"</div><div class=\"col-md-3\">"+game+"</div><div class=\"col-md-3\">"+viewers+"</div></div>");
+            }
+        })
+    }
+
     function getStatus(user) {
         $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?',function(data) {
             if (data.error) {
                 $(".invalid").append("<p>"+user+"</p>")
             } else getInfo(user);
-        })
-        
-    }
-    function getInfo(user) {
-        var logo = "";
-        var link = "";
-        var name = "";
-        $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?', function (data) {
-            console.log(data);
-            name= data.display_name;
-            logo = data.logo;
-            link = data.url;
-            followers = data.followers;
-            /*$(".userArea").append('<div class="row"><div class="col-md-2"><img src='+logo+'</div>
-            <div class="col-md-3">'+name+'</div>
-            <div class="col-md-2"></div>
-            <div class="col-md-3">Playing:</div>
-            <div class="col-md-2">'+followers+'</div>
-        </div>'*/
         })
     }
     
