3c62948787861474c5f9fd29528fd68c629af57f
robgaston1 <robcoach0221@gmail.com>
2017-01-16 13:29:44 -0500

    adding a second get request. Splitting the display into active users and not found users

--- twitch.js
+++ twitch.js
@@ -1,32 +1,37 @@
 $(document).ready(function() {
 
-    var userName = "";
-    var userUrl = "";
-    var status = "";
-    var game = "";
-    var viewers = 0;
+
     var userArray = ["freecodecamp", "brunofin", "streamerhouse", "insomniacgamers12345", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     
+    function getStatus(user) {
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?',function(data) {
+            if (data.error) {
+                $(".invalid").append("<p>"+user+"</p>")
+            } else getInfo(user);
+        })
+        
+    }
     function getInfo(user) {
         var logo = "";
         var link = "";
-        $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
+        var name = "";
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?', function (data) {
             console.log(data);
+            name= data.display_name;
             logo = data.logo;
             link = data.url;
-            followers = data.followers
-            $(".userArea").append('<div class="row">
+            followers = data.followers;
+            /*$(".userArea").append('<div class="row"><div class="col-md-2"><img src='+logo+'</div>
+            <div class="col-md-3">'+name+'</div>
             <div class="col-md-2"></div>
-            <div class="col-md-3">Stream Name:</div>
-            <div class="col-md-2">Status:</div>
             <div class="col-md-3">Playing:</div>
-            <div class="col-md-2">Viewers:</div>
-        </div>')
-
-        });
+            <div class="col-md-2">'+followers+'</div>
+        </div>'*/
+        })
     }
-/*  for (i = 0; i < userArray.length; i++) {
-    getInfo(userArray[i]);
-  }*/
+    
+    for (i = 0; i < userArray.length; i++) {
+    getStatus(userArray[i]);
+  }
 
 }); //end of $(document).ready function
