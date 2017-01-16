0d1d0377328b386de232e06abf5536f2d79f19f9
robgaston1 <robcoach0221@gmail.com>
2017-01-15 20:16:06 -0500

    adding some updates to the

--- twitch.js
+++ twitch.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
 
-  var userName = "";
-  var userUrl = "";
-  var status = "";
-  var game = "";
-  var viewers = 0;
-  var userArray = ["freecodecamp", "brunofin", "streamerhouse", "insomniacgamers12345", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
+    var userName = "";
+    var userUrl = "";
+    var status = "";
+    var game = "";
+    var viewers = 0;
+    var userArray = ["freecodecamp", "brunofin", "streamerhouse", "insomniacgamers12345", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
-    
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
+    function getInfo(user) {
+        var logo = "";
+        var link = "";
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
+            console.log(data);
+            logo = data.logo;
+            link = data.url;
+            followers = data.followers
+            $(".userArea").append('<div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-3">Stream Name:</div>
+            <div class="col-md-2">Status:</div>
+            <div class="col-md-3">Playing:</div>
+            <div class="col-md-2">Viewers:</div>
+        </div>')
 
+        });
+    }
 /*  for (i = 0; i < userArray.length; i++) {
     getInfo(userArray[i]);
   }*/
